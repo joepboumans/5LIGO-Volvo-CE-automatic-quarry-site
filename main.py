d65d2587f3eb2f29f201e67b49b28c20a4b38f51
joepboumans <joep_boumans@live.nl>
2023-11-15 09:47:27 +0100

    Parse config via regex

---
+++ main.py
@@ -0,0 +1,35 @@
+import re
+
+def check_for_match(match):
+    if match:
+        return match.group(1)
+    else:
+        return 0
+    
+def read_config(file='config.txt'):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        # Get the number of items
+        nHaulers = re.search(r'(\d+)', lines[0]).group(1)
+        nLP = re.search(r'(\d+)', lines[1]).group(1)
+        nULP = re.search(r'(\d+)', lines[2]).group(1)
+        nSO = re.search(r'(\d+)', lines[3]).group(1)
+        nCS = re.search(r'(\d+)', lines[4]).group(1)
+        
+        # Get their positions
+        init_haulers = re.findall(r'\[(\d+),(\d+)\]', lines[5])
+        LP_positions = re.findall(r'\[(\d+),(\d+)\]', lines[6])
+        ULP_positions = re.findall(r'\[(\d+),(\d+)\]', lines[7])
+        SO_positions = re.findall(r'\[(\d+),(\d+)\]', lines[8])
+        CS_positions = re.findall(r'\[(\d+),(\d+)\]', lines[9])
+        
+        # Get the max battery capacity and initial energy
+        max_energy = check_for_match(re.findall(r'(\d+)', lines[10]))
+        initial_energy = check_for_match(re.findall(r'(\d+)', lines[11]))
+        
+    return nHaulers, nLP, nULP, nSO, nCS, init_haulers, LP_positions, ULP_positions, SO_positions, CS_positions, max_energy, initial_energy
+    
+def read_mission(file='mission.txt'):
+    pass
+
+read_config()
